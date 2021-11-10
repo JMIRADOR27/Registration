ea8f7e89f0c3d81476867e1f82d22cbc3e6c35ab
Renzo <renzo.amoranto@coexstar.ph>
2021-11-10 15:51:34 +0800

    username commit

--- actions/function.php
+++ actions/function.php
@@ -14,22 +14,25 @@ $message = 'Registration Failed';
 function insertRegistrants($fname, $lname,$mname,$age,$cnumber,$email,$address,$RFID) {
     global $status,$conn,$message;
 
+
+	$username = $fname[0].$lname.mt_rand(2000,9000);
+
 	
-	$stmt = $conn->prepare('INSERT INTO registrants (first_name, last_name, middle_name, age, contact_number, email,RFID,address) VALUES (?,?,?,?,?,?,?,?,?)');
+	$stmt = $conn->prepare('INSERT INTO registrants (first_name, last_name, middle_name, age, contact_number, email,RFID,address,Username) VALUES (?,?,?,?,?,?,?,?,?)');
 
 	// using prepared statement several times with different variables
 	if (
 		$stmt &&
-		$stmt->bind_param('sssisisss', $fname, $lname,$mname,$age,$cnumber,$email,$RFID,$address) &&
+		$stmt->bind_param('sssisisss', $fname, $lname,$mname,$age,$cnumber,$email,$RFID,$address,$username) &&
 		$stmt->execute()
 	) {
-		if(emailer($email)){
+		// if(emailer($email)){
 	 	 $status = 1;
 		 $message = "Successfully Registered";
-		}else{
-		 $status = 0;
-		 $message = "Failed to sent email verification.";
-		}
+		// }else{
+		//  $status = 0;
+		//  $message = "Failed to sent email verification.";
+		// }
 	}
 }
 
@@ -47,26 +50,26 @@ function emailValidation($email){
 }
 
 function emailer($email){
-	$mail = new PHPMailer;
-	$mail->isSMTP();
-	$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
-	$mail->Port = 587; // TLS only
-	$mail->SMTPSecure = 'tls'; // ssl is depracated
-	$mail->SMTPAuth = true;
-	$mail->Username = "Email";
-	$mail->Password = "Password";
-	$mail->setFrom("Youremail@gmail.com");
-	$mail->addAddress($email);
-	$mail->Subject = 'PHPMailer GMail SMTP test';
-	$mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
-	$mail->AltBody = 'HTML messaging not supported';
-	// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+	// $mail = new PHPMailer;
+	// $mail->isSMTP();
+	// $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+	// $mail->Port = 587; // TLS only
+	// $mail->SMTPSecure = 'tls'; // ssl is depracated
+	// $mail->SMTPAuth = true;
+	// $mail->Username = "Email";
+	// $mail->Password = "Password";
+	// $mail->setFrom("Youremail@gmail.com");
+	// $mail->addAddress($email);
+	// $mail->Subject = 'PHPMailer GMail SMTP test';
+	// $mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+	// $mail->AltBody = 'HTML messaging not supported';
+	// // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
 	
-	if(!$mail->send()){
-		echo "Mailer Error: " . $mail->ErrorInfo;
-	}else{
-		echo "Message sent!";
-	}
+	// if(!$mail->send()){
+	// 	echo "Mailer Error: " . $mail->ErrorInfo;
+	// }else{
+	// 	echo "Message sent!";
+	// }
 }
 
 ?>
