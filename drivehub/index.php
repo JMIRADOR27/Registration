6d4b8cacdc898f828f6e36014aac3c7d93e0bf18
Marc Englis <englis.marc@icloud.com>
2022-02-03 16:46:48 +0800

    favicon change

--- drivehub/index.php
+++ drivehub/index.php
@@ -7,7 +7,8 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>DriveHub - MPTC Mobility</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- <link rel="icon" type="image/x-icon" href="assets/favicon.ico" /> -->
+    <link rel="shortcut icon" href="assets/favicon.ico">
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Google fonts-->
